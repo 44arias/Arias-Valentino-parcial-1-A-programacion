102a5404def14041a6d809817172463c095fd40a
44arias <valentinoarias10@gmail.com>
2023-05-22 10:44:24 -0300

    Parcial Terminado

--- funcion1.py
+++ funcion1.py
@@ -2,7 +2,7 @@
 # 1. Crear una función llamada aplicarAumento que reciba como parámetro el precio de un producto y 
 # devuelva el valor del producto con un aumento del 5%. Realizar la llamada desde el main.
 
-def aplicarAumento(precio: int) -> float:
+def aplicarAumento(precio: int):
     aumento = precio * 0.05  # Calcula el aumento como el 5% del precio original
     precio_con_aumento = precio + aumento  # Calcula el precio con el aumento 
     return precio_con_aumento
--- funcion3.py
+++ funcion3.py
@@ -1,7 +1,6 @@
 # 3. Crear una función que se llame ordenarCaracteres que reciba una cadena de caracteres como parámetro  y su responsabilidad 
 # es ordenar los caracteres de manera ascendente dentro de la cadena. Ejemplo si le pasamos "algoritmo" la deja como "agilmoort"
 
-
 def ordenarCaracteres(cadena: str):
     caracteres = list(cadena)  # Convierte la cadena en una lista de caracteres
     caracteres.sort()  # Ordena de manera ascendente
