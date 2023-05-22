3970ffb56b9caf89cdafca598e3c4ed63f182ee0
44arias <valentinoarias10@gmail.com>
2023-05-22 10:42:23 -0300

    parcial terminado

--- funcion2.py
+++ funcion2.py
@@ -3,6 +3,7 @@
 # como primer parámetro, un carácter como segundo y otro carácter  como tercero,  la misma deberá 
 # reemplazar cada ocurrencia del segundo parámetro por el tercero y devolver la cantidad de veces 
 # que se reemplazo ese carácter  en la cadena.
+
 def reemplazarCaracteres(cadena: str, caracter_a_reemplazar: str, caracter_reemplazo: str):
     cadena_reemplazada = cadena.replace(caracter_a_reemplazar, caracter_reemplazo)
     cantidad_reemplazos = 0
